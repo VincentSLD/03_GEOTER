a49295a1a96d5fbf0f50b4b355a64618f62e0716
VincentSLD <vsalaud@be-gph.fr>
2026-02-28 17:20:47 +0100

    sandages

--- lancer-ia.bat
+++ lancer-ia.bat
@@ -1,4 +1,4 @@
 @echo off
-set ANTHROPIC_API_KEY=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
+set ANTHROPIC_API_KEY=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
 python proxy-ia.py
 pause
